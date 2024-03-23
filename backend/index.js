9620d2300c323698d4870ab185a5cc96ea53f2b1
Koushik <gurramkoushik8@gmail.com>
2024-03-23 11:42:34 +0530

    backend

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 // using this our react will connect to the backend
 app.use(cors());
 
-mongoose.connect('mongodb://localhost:27017');
+mongoose.connect('mongodb://localhost:27017/');
 
 app.get("/",(req,res)=>{
     res.send("Express is running");
@@ -80,8 +80,45 @@ app.post('/login',async(req,res)=>{
         res.json({success:false,errors:"Wrong Email Id"})
     }
 })
-// API creation
 
+
+const request = require('request');
+
+
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// POST endpoint to handle /challans
+app.post('/challans', (req, res) => {
+  const apiKey = req.header('096ba636-90b7-4c10-842c-15f1f655d474');
+  const accountId = req.header('07fb0057ed0c/d1add25b-4bf1-4b51-a659-8c523dc282ad');
+  const externalApiUrl = 'https://eve.idfy.com/v3/tasks/async/verify_with_source/aadhaar_lite'; // Replace with your external API URL
+  const requestData = req.body;
+
+  // Make a request to the external API
+  const options = {
+    method: 'POST',
+    url: externalApiUrl,
+    headers: {
+      'api-key': apiKey,
+      'account-id': accountId,
+      
+    },
+    body: JSON.stringify(requestData)
+  };
+
+  request(options, (error, response, body) => {
+    if (error) {
+      console.error('Error:', error);
+      res.status(500).send('Error making request to external API');
+      return;
+    }
+
+    console.log('Response from external API:', body);
+    res.send(body);
+  });
+});
 
 
 
